c0f43f58da75911d0f6e5a73273f020eda0cf298
jayanth-1 <apoorjayanth1@gmail.com>
2018-07-10 01:27:25 +0530

    separated into folders

--- Java-assignment/Assignment2/TestAlphabets.java
+++ Java-assignment/Assignment2/TestAlphabets.java
@@ -12,7 +12,7 @@ public class TestAlphabets {
 	@Test
 	public static void main(String[] args) {
 
-        Alphabets alphabet = new Alphabets();
+                Alphabets alphabet = new Alphabets();
                 assertEquals(alphabet.checkingAlphabets("@!abcdefghijklmnopqrstuvwxyz"),"String contains all alphabets a-z(case insensitive) \n");
 		assertEquals(alphabet.checkingAlphabets("ftgybj"),"String doesn't contains 26 alphabets");
 		assertEquals(alphabet.checkingAlphabets(""),"String doesn't contains 26 alphabets");
