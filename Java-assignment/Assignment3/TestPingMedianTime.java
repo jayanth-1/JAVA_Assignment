fe05eb7fd154bfbe1d62492f08b2491385c8c616
jayanth-1 <apoorjayanth1@gmail.com>
2018-07-10 01:30:25 +0530

    separated into folders

--- Java-assignment/Assignment3/TestPingMedianTime.java
+++ Java-assignment/Assignment3/TestPingMedianTime.java
@@ -9,22 +9,22 @@ class TestPingMedianTime{
 	  public static void main(String[] args)
 	  {
 	     List<String> neg_testcase = Arrays.asList("0.0","0.85.0");
-         List<String> pos_testcase = Arrays.asList("127.0.0.1", "101.0.0.1");
-         int i;
-         PingMedianTime pingObj = new PingMedianTime();
-         System.out.println("Running Positive test case \n\n");
-         for(i=0;i<pos_testcase.size();i++)
-         {
-            System.out.println("test case " + i + " " + pos_testcase.get(i) + "\n");
-            System.out.println(pingObj.computeMedian(pos_testcase.get(i),2));
-            System.out.println("\n");
+             List<String> pos_testcase = Arrays.asList("127.0.0.1", "101.0.0.1");
+             int i;
+             PingMedianTime pingObj = new PingMedianTime();
+             System.out.println("Running Positive test case \n\n");
+             for(i=0;i<pos_testcase.size();i++)
+             {
+               System.out.println("test case " + i + " " + pos_testcase.get(i) + "\n");
+               System.out.println(pingObj.computeMedian(pos_testcase.get(i),2));
+               System.out.println("\n");
 
     }
-         for(i=0;i<neg_testcase.size();i++)
-         {
-            System.out.println("test case " + i + " " + neg_testcase.get(i) + "\n");
-            System.out.println(pingObj.computeMedian(neg_testcase.get(i),2));
-            System.out.println("\n");
+             for(i=0;i<neg_testcase.size();i++)
+             {
+                System.out.println("test case " + i + " " + neg_testcase.get(i) + "\n");
+                System.out.println(pingObj.computeMedian(neg_testcase.get(i),2));
+                System.out.println("\n");
 
     }
 
--- Java-assignment/Assignment4/TestKyc.java
+++ Java-assignment/Assignment4/TestKyc.java
@@ -13,11 +13,11 @@ public class TestKyc {
 	public static void main(String[] args) throws Exception{
 
         
-        assertEquals(Kyc.rangeCalculation("16-07-1998","27-06-2017"),"16-06-2017 27-06-2017");
-        assertEquals(Kyc.rangeCalculation("04-02-2016","04-04-2017"),"05-01-2017 06-03-2017");
-        assertEquals(Kyc.rangeCalculation("04-05-2017","04-04-2017"),"No range");
-        assertEquals(Kyc.rangeCalculation("04-04-2015","04-04-2016"),"05-03-2016 04-04-2016");
-        assertEquals(Kyc.rangeCalculation("04-04-2015","15-03-2016"),"05-03-2016 15-03-2016");
+          assertEquals(Kyc.rangeCalculation("16-07-1998","27-06-2017"),"16-06-2017 27-06-2017");
+          assertEquals(Kyc.rangeCalculation("04-02-2016","04-04-2017"),"05-01-2017 06-03-2017");
+          assertEquals(Kyc.rangeCalculation("04-05-2017","04-04-2017"),"No range");
+          assertEquals(Kyc.rangeCalculation("04-04-2015","04-04-2016"),"05-03-2016 04-04-2016");
+          assertEquals(Kyc.rangeCalculation("04-04-2015","15-03-2016"),"05-03-2016 15-03-2016");
 
 		
 	}
