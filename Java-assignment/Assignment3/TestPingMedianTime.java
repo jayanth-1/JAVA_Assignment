6852c5a54ef96de9a9a4f23ac80e09409e10fa27
Jayanth kumar Apoor <apoorjayanth1@gmail.com>
2018-07-17 20:44:39 +0530

    changes done

--- Java-assignment/Assignment3/TestPingMedianTime.java
+++ Java-assignment/Assignment3/TestPingMedianTime.java
@@ -14,29 +14,29 @@ public class TestPingMedianTime {
 
 		assertFalse(PingMedianTime.validateString("0.0.1"));
 
-}
+        }
 
     @Test
 	public void positiveTestValidate2() {
 
 		assertTrue(PingMedianTime.validateString("127.0.0.1"));
 
-}
+        }
 
     @Test
 	public void testOddPing() {
-        double d1 = 0.9;
+                double d1 = 0.9;
 		assertEquals(d1,PingMedianTime.oddPingTime("0.9"),0.1);
 
-}
+        }
 
     @Test
 	public void testEvenPing() {
-        double d1 = 0.9;
-        double d2 = 0.7;
-        double d3 = (d1 + d2)/2;
+                double d1 = 0.9;
+                double d2 = 0.7;
+                double d3 = (d1 + d2)/2;
 		assertEquals(d3,PingMedianTime.evenPingTime("0.9","0.7"),0.1);
 
-}
+        }
 
 }
